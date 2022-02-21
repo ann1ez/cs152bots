0b2785113769e214c2e82399ed39972deb18d8b3
Annie Zhu <44847825+ann1ez@users.noreply.github.com>
2022-02-21 14:14:06 -0800

    Update report.py
    Updating the report flow to execute the majority of the user reporting side.

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -6,6 +6,9 @@ class State(Enum):
     REPORT_START = auto()
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
+    BROAD_CAT_IDENTIFIED = auto()
+    SPECIFIC_CAT_IDENTIFIED = auto()
+    FINAL_MESSAGE = auto()
     REPORT_COMPLETE = auto()
 
 class Report:
@@ -17,6 +20,9 @@ class Report:
         self.state = State.REPORT_START
         self.client = client
         self.message = None
+        self.broadCategory = None
+        self.specificCategory = None
+        self.optionalMessage = None
     
     async def handle_message(self, message):
         '''
@@ -55,18 +61,68 @@ class Report:
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+            reply =  "Enter `1` for Misinformation\n"
+            reply += "Enter `2` for Dangerous or Illegal Content\n"
+            reply += "Enter `3` for Harrassemnt or Abuse.\n"
+            reply += "Enter `4` for More Options\n"
+            reply += "Enter `5` for I don't want to see this content"
+            self.state = State.BROAD_CAT_IDENTIFIED
+            return ["Great, I found this message:", "```" + message.author.name + ": " + message.content + "```", \
+                    "What is the reason you are reporting this message? (Choose from below.)\n" + reply]
+          
+        if self.state == State.BROAD_CAT_IDENTIFIED:
+            # TODO: store this information about what broad category of abuse the message falls under
+            self.broadCategory = message.content
+            self.state = State.SPECIFIC_CAT_IDENTIFIED
+            if message.content == '1':
+                reply =  "Enter `1` for Elections\n"
+                reply += "Enter `2` for Covid-19\n"
+                reply += "Enter `3` for Other Health or Medical\n"
+                reply += "Enter `4` for Climate Change\n"
+                reply += "Enter `5` for Gun Violence\n"
+                reply += "Enter `6` for Other"
+                return ["What kind of misinformation is this? (Choose from below).\n" + reply]
+            if message.content == '2':
+                reply =  "Enter `1` for Expresses intentions of self-harm or suicide\n"
+                reply += "Enter `2` for Expresses intentions for harming others\n"
+                reply += "Enter `3` for Dangerous or Violent Organizations\n"
+                reply += "Enter `4` for Child Sexual Abuse Materials\n"
+                reply += "Enter `5` for Human Trafficking\n"
+                reply += "Enter `6` for Sale of Illegal Goods"
+                return ["What kind of dangerous or illegal content is this? (Choose from below).\n" + reply]
+            if message.content == '3':
+                reply =  "Enter `1` for Hate Speech or Symbols\n"
+                reply += "Enter `2` for Bullying\n"
+                reply += "Enter `3` for Sexual Harrassment\n"
+                reply += "Enter `4` for Stalking\n"
+                return ["What kind of harrassement of abuse is this? (Choose from below).\n" + reply]
+            if message.content == '4':
+                reply =  "Enter `1` for Spam\n"
+                reply += "Enter `2` for Copy Right Infringement\n"
+                reply += "Enter `3` for Impersonation\n"
+                reply += "Enter `4` for Other\n"
+                return ["Here are more options: (Choose from below).\n" + reply]
         
-        if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+        if self.state == State.SPECIFIC_CAT_IDENTIFIED:
+            self.specificCategory = message.content
+            reply = ""
+            if (self.broadCategory == '1') and (self.specificCategory == '2'):
+                reply = "\n\nAlso, here is the link to visit the CDC website for the latest information on Covid-19: https://www.cdc.gov/coronavirus/2019-ncov/index.html" 
+            self.state = State.FINAL_MESSAGE
+            return["If you would like to add more information to your report, here is space to do so. Enter your message when you are ready to proceed." + reply]
+        
+        if self.state == State.FINAL_MESSAGE:
+            self.optionalMessage = message.content
+            self.state = State.REPORT_COMPLETE
+            if self.broadCategory == '1':
+                return["Thank you for your report. We will send this to our fact-checking partners and when misinformation is confimed, we will limit the content's distribution and warn other users."]
+            if self.broadCategory== '2':
+                return["Thank you for your report. It will be reviewed by our content moderation team, who will decide future action, including any necessary reports to law enforcement. Thank you for trying to keep our platform safe."]
+            return["Thank you for your report. It will be reviewed by our content moderation team, who will decide future action, including if the post should be removed or the user banned."]
 
         return []
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
-    
-
-
     
 
